b37157b3cfeaf9cda865c5fa61e422bf7a0abd19
blixt <teodor.blixt@gmail.com>
2021-01-11 13:27:23 +0100

    added additem and remove item functionality to checkout

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
+import {
+	clearItemFromCart,
+	addItem,
+	removeItem,
+} from '../../redux/cart/cart.actions';
 
 const CheckoutCont = styled.div`
 	width: 100%;
@@ -23,25 +29,43 @@ const Details = styled.span`
 `;
 const Quantity = styled.span`
 	width: 23%;
-	padding-left: 20px;
+	display: flex;
 `;
 
 const RemoveButton = styled.div`
 	cursor: pointer;
 	padding-left: 12px;
 `;
-const CheckoutItem = ({ cartItem: { name, quantity, imageUrl, price } }) => {
+const Value = styled.span`
+	margin: 0 10px;
+`;
+const Arrow = styled.div`
+	cursor: pointer;
+`;
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+	const { name, imageUrl, price, quantity } = cartItem;
 	return (
 		<CheckoutCont>
 			<ImageCont>
 				<Image src={imageUrl} alt='item' />
 			</ImageCont>
 			<Details>{name}</Details>
-			<Quantity>{quantity}</Quantity>
+			<Quantity>
+				<Arrow onClick={() => removeItem(cartItem)}>&#10094;</Arrow>
+				<Value>{quantity}</Value>
+
+				<Arrow onClick={() => addItem(cartItem)}>&#10095;</Arrow>
+			</Quantity>
 			<Details>${price}</Details>
-			<RemoveButton>&#10005;</RemoveButton>
+			<RemoveButton onClick={() => clearItem(cartItem)}>&#10005;</RemoveButton>
 		</CheckoutCont>
 	);
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+	clearItem: item => dispatch(clearItemFromCart(item)),
+	addItem: item => dispatch(addItem(item)),
+	removeItem: item => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
